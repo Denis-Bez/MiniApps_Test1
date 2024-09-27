d990fae06dd30886de93ec8176b3d6ef4f01dbf8
Denis Bezvershenko <v417459@yandex.ru>
2024-09-27 15:34:52 +0500

    Advanced test

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 # @world_cadabra_bot
+# https://docs.ton.org/develop/dapps/telegram-apps/app-examples
+# https://docs.ton.org/participate/web3/dns
+# https://docs.ton.org/develop/dapps/tutorials/how-to-run-ton-site
+
 TOKEN_BOT = "5871108454:AAEL3Fcb7hlT4I-jL-H3zWwckbyO1HxMqoQ"
